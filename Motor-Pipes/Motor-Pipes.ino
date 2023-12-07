59417fc0aa8017140cceb5cf9f929a63fd3d37bc
NiclasMuss <39119681+NiclasMuss@users.noreply.github.com>
2023-12-07 14:59:05 +0100

    Motor-Pipes skeleton
    basic structure of the Motor-Pipes Project

---
+++ Motor-Pipes/Motor-Pipes.ino
@@ -0,0 +1,18 @@
+// include the library code
+//TODO: Motor Library
+
+#define BUTTONPIN 6 //Arduino pin tiesd to Button
+
+bool button = false
+
+void setup() {
+  pinMode(BUTTONPIN,INPUT); //initialize vibration switch as an input
+  //TODO: Motor setup
+}
+
+void loop() {
+  //TODO: check Button high low
+  if(button){
+    //TODO: Rotate
+  }
+}
