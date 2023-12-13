60feda24573664cd3276d250a4b2ede9ce3c4ee1
NiclasMuss <39119681+NiclasMuss@users.noreply.github.com>
2023-12-13 14:52:23 +0100

    Transformed into a state machine

--- Ultrasonic-Projector/Ultrasonic-Projector.ino
+++ Ultrasonic-Projector/Ultrasonic-Projector.ino
@@ -17,11 +17,14 @@ NewPing sonar3(TRIGGER_PIN_3, ECHO_PIN_3, MAX_DISTANCE); // NewPing setup of pin
 int dist1 = 0;
 int dist2 = 0;
 int dist3 = 0;
+int lastSensor = 3;
+int highSensor = 0;
+bool change = false;
 
 TVout TV;
 
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  Serial.begin(9600); // Open serial monitor at 115200 baud to see ping results.
   TV.begin(PAL, 128, 96);
 }
 
@@ -47,21 +50,56 @@ void loop() {
   Serial.print(dist3);
   Serial.print("cm | ");
 
+  highSensor = sensorcheck();
+
+  if (highSensor != lastSensor){
+    lastSensor = highSensor;
+    change = true;
+  }
+
+  switch(lastSensor) {
+    case 0:
+      // no change 
+      break;
+    case 1: //closest sensor #941100 (ROT)
+      TV.clear_screen();
+      TV.draw_rect(20, 20, 40, 40, WHITE);
+      break;
+    case 2: //#7030A0 (VIOLETT)
+      TV.clear_screen();
+      TV.draw_circle(40, 40, 20, WHITE);
+      break;
+    case 3: //#005493 (DUNKELBLAU)
+      TV.clear_screen();
+      TV.draw_column(60, 60, 20, WHITE);
+      break;
+      case 4: //#8DD4FB (HELLBLAU)
+      //TODO
+      break;
+      case 5: //furthest Sensor #FFFFFF (WEIẞ)
+      //TODO
+      break;
+    default:
+      break;
+  }
+  if (change){
+    delay (400);
+    change = false;
+  }
+}
+
+int sensorcheck() {
   if (dist1 < DETECTION_DISTANCE){ //closest sensor
-    Serial.println("red");
-    TV.clear_screen();
-    TV.draw_rect(20, 20, 40, 40, WHITE);
+    Serial.println("sensor 1");
+    return 1;
   } else if (dist2 < DETECTION_DISTANCE) {
-    Serial.println("green");
-    TV.clear_screen();
-    TV.draw_circle(40, 40, 20, WHITE);
+    Serial.println("sensor 2");
+    return 2;
   } else if (dist3 < DETECTION_DISTANCE) {
-    Serial.println("blue");
-    TV.clear_screen();
-    TV.draw_column(60, 60, 20, WHITE);
+    Serial.println("sensor 3");
+    return 3;
   } else { //no sensor detectects anything
-  TV.clear_screen();
-  //TODO: maybe less clear_screen
-  Serial.println("");
+    Serial.println("no sensor"); //TODO: Something about this makes the screen flicker
+    return 0;
   }
 }
