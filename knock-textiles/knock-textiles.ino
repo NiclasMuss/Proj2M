3e9d8704fbdba5990c6755cd3ec63e3bfc179d6f
NiclasMuss <39119681+NiclasMuss@users.noreply.github.com>
2023-12-15 19:35:46 +0100

    Update knock-textiles

--- knock-textiles/knock-textiles.ino
+++ knock-textiles/knock-textiles.ino
@@ -1,14 +1,34 @@
 // include the library code
-//TODO: Sound-library
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
 
-#define KNOCKPIN        8    // Arduino pin tied to the Knock-Sensor
+#define KNOCKPIN  8    // Arduino pin tied to the Knock-Sensor
+#define MP3LENGTH 10000 // Length of the Mp3 file in Milliseconds
 
 int shock = 0; //initialize the variable shock as 0 TODO: could use for error handling
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
 
 void setup() {
   
   pinMode(KNOCKPIN,INPUT); //initialize Knock-Sensor as an input
+  mySoftwareSerial.begin(9600);
+  Serial.begin(57600);
 
+  Serial.println();
+  Serial.println(F("DFRobot DFPlayer Mini Demo"));
+  Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
+  
+  if (!myDFPlayer.begin(mySoftwareSerial)) {  //Use softwareSerial to communicate with mp3.
+    Serial.println(F("Unable to begin:"));
+    Serial.println(F("1.Please recheck the connection!"));
+    Serial.println(F("2.Please insert the SD card!"));
+    while(true);
+  }
+  Serial.println(F("DFPlayer Mini online."));
+  myDFPlayer.volume(2);  //Set volume value. From 0 to 30
 }
 
 void loop() {
@@ -19,7 +39,7 @@ void loop() {
 
   if(shock == LOW)  //with vibration signal
   {
-    //TODO: Sound
+    myDFPlayer.play(1);  //Play the first mp3
+    delay(MP3LENGTH);
   }
-
 }
