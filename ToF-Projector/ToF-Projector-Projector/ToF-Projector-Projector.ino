c08df6952fa9a7f4f792bbb77f79cd2b94ed4f95
NiclasMuss <niclas.muss@yahoo.de>
2024-02-06 00:12:58 +0100

    functioning 5 colours tof projector

--- ToF-Projector/ToF-Projector-Projector/ToF-Projector-Projector.ino
+++ ToF-Projector/ToF-Projector-Projector/ToF-Projector-Projector.ino
@@ -1,11 +1,12 @@
 // include the library code
 #include <Wire.h>
 #include <VL53L1X.h>
+
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
 
-#define DETECTION_DISTANCE 1300 //Distance that counts as detection
+#define DETECTION_DISTANCE 3000 //Distance that counts as detection
 
 //I/O Constants
 #define IN_1      14
@@ -13,10 +14,10 @@
 #define IN_3      16
 #define IN_4      17
 
-//Mp3-Variables
 SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
-int volume = 1;
+int volume = 10;
+unsigned long startTime = 0;
 
 //ToF variables
 VL53L1X sensor;
@@ -56,7 +57,6 @@ void setup() {
     Serial.print("Failed to detect and initialize sensor!");
     while (1);
   }
-  //maybe use long distance in example?
 
   // Start continuous readings at a rate of one measurement every 50 ms (the
   // inter-measurement period). This period should be at least as long as the
@@ -66,7 +66,6 @@ void setup() {
 
 void loop() {
   dist = sensor.read();
-  //Serial.print(dist);
   if (sensor.timeoutOccurred()) { Serial.print(" TIMEOUT"); }
 
   //check own Sensor
@@ -90,20 +89,17 @@ void loop() {
     if (areSensorsAdjacent(highSensor, lastSensor)){
       if(highSensor<lastSensor){ //a new sensor closer to the start has been trigered
       Serial.println("#S|SENDK|[0&{LEFT}]#");
-      //turn the volume down
-      volume = volume -2;
-      myDFPlayer.volume(volume);  
+      volume = volume -3;
+      myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
     }
       else{ //a new sensor closer to the end has been trigered
       Serial.println("#S|SENDK|[0&{RIGHT}]#");
-      //turn the volume up
-      volume = volume +2;
+      volume = volume +3;
       myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
     }
     lastSensor = highSensor;
     change = true;
     }
-    
   }
 
   //Debug Grid
@@ -120,29 +116,46 @@ void loop() {
 
 int sensorcheck() {
   if (sensor4){ //futhest sensor
-  //  Serial.println("sensor 4");
     return 5;
   } else if (sensor3) {
-  //  Serial.println("sensor 3");
     return 4;
   } else if (sensor2) {
-  //  Serial.println("sensor 2");
     return 3;
   } else if (sensor1) {
-  //  Serial.println("sensor 1");
     return 2;
   } else if (sensor0) { //closest sensor
-  //  Serial.println("sensor self");
+  startTime = millis();
     return 1;
+
   } else { //no sensor detectects anything
-  //  Serial.println("no sensor"); 
+    if (millis() - startTime > 2000  && lastSensor ==1){
+      lastSensor = -1;
+      Serial.println("#S|SENDK|[0&{LEFT}]#");
+      volume = volume -2;
+      myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
+    }
     return 0;
   }
 }
 
 bool areSensorsAdjacent(int sensor1, int sensor2) {
-  if (sensor2 == 1.5){
+  if (sensor2 == -1){
+
     //handle special State
+    if (sensor1=1){
+      Serial.println("#S|SENDK|[0&{RIGHT}]#");
+      lastSensor=1;
+      volume = volume +3;
+      myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
+      return false;
+    }else if (sensor1 = 2){
+      Serial.println("#S|SENDK|[0&{RIGHT}]#");
+      Serial.println("#S|SENDK|[0&{RIGHT}]#");
+      lastSensor=2;
+      volume = volume +6;
+      myDFPlayer.volume(volume);  //Set volume value. From 0 to 30
+      return false;
+    }
   }
   return abs(sensor1 - sensor2) == 1;
 }
